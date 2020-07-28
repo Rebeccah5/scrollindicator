20402aad78fbffb2745c8119232142b35afd4bdc
Rebeccah5 <wambuirebeka@gmail.com>
2020-07-28 17:30:42 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+alert("Try Scrolling the page up and down.");
+
+window.onscroll = function() {
+    var pos =  document.documentElement.scrollTop;
+    var calc_height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scroll = pos * 100 / calc_height;
+    document.getElementById("progress").style.width= scroll + "%";
+}
